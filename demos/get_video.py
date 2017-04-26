1de0c18f4424091d79a797d8d31a56f9a4083151
triyono, ibnu <ibnu.triyono@gmail.com>
2017-04-26 18:48:11 +0700

    add demo for get video

---
+++ demos/get_video.py
@@ -0,0 +1,16 @@
+import sys
+
+from Ziggeo import Ziggeo
+
+if(len(sys.argv) < 4):
+	print "Error\n"
+	print "Usage: $>python delete.py YOUR_API_TOKEN YOUR_PRIVATE_KEY VIDEO_TOKEN\n"
+	sys.exit()
+
+api_token = sys.argv[1]
+private_key = sys.argv[2]
+video_token = sys.argv[3]
+
+ziggeo = Ziggeo(api_token, private_key)
+
+print ziggeo.videos().get(video_token)
