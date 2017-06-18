5022c372fcaafa2b5a45fef8a7cea09aa5d0239f
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-06-19 01:41:44 +0700

    add download all videos script

---
+++ demos/download_all_videos.py
@@ -0,0 +1,29 @@
+import sys
+
+from Ziggeo import Ziggeo
+
+if(len(sys.argv) < 3):
+	print "Error\n"
+	print "Usage: $>python download_all_videos.py YOUR_API_TOKEN YOUR_PRIVATE_KEY\n"
+	sys.exit()
+
+api_token = sys.argv[1]
+private_key = sys.argv[2]
+ziggeo = Ziggeo(api_token, private_key)
+def indexVideos(skip=0):
+	yey = 0
+	video_list = ziggeo.videos().index({"limit":100, "skip":skip})
+	for video in video_list:
+		download_video_token = video["token"]
+		print "downloading video "+download_video_token
+		stream_token = video["default_stream"]["token"]
+		file_name = download_video_token+"_"+stream_token+'.'+video["default_stream"]["video_type"]
+
+		file = open(file_name, "wb")
+		downloaded_file = ziggeo.streams().download_video(download_video_token, stream_token)
+		file.write(downloaded_file)
+		file.close()
+	if(len(video_list) > 0):
+		indexVideos(skip+100)
+	pass
+indexVideos(0)
