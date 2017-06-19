28e40535c2f834adc385194aa38cd4d651bfe96b
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-06-20 04:36:36 +0700

    add script to copy all videos from one application to other

---
+++ demos/copy_application.py
@@ -0,0 +1,39 @@
+import sys
+
+from Ziggeo import Ziggeo
+
+if(len(sys.argv) < 5):
+	print "Error\n"
+	print "Usage: $>python copy_application.py SOURCE_API_TOKEN SOURCE_PRIVATE_KEY TARGET_API_TOKEN TARGET_PRIVATE_KEY\n"
+	sys.exit()
+
+source_api_token = sys.argv[1]
+source_private_key = sys.argv[2]
+target_api_token = sys.argv[3]
+target_private_key = sys.argv[4]
+ziggeo_source = Ziggeo(source_api_token, source_private_key)
+ziggeo_target = Ziggeo(target_api_token, target_private_key)
+def indexVideos(skip=0):
+	yey = 0
+	video_list = ziggeo_source.videos().index({"limit":2, "skip":skip})
+	for video in video_list:
+		download_video_token = video["token"]
+		print "downloading video "+download_video_token
+		stream_token = video["original_stream"]["token"]
+		file_name = download_video_token+"_"+stream_token+'.'+video["original_stream"]["video_type"]
+		# We are using "wb" for write binary. To prevent windows from using write as non binary
+		file = open(file_name, "wb")
+		downloaded_file = ziggeo_source.streams().download_video(download_video_token, stream_token)
+		file.write(downloaded_file)
+		file.close()
+		uploadVideo(file_name)
+	if(len(video_list) > 0):
+		indexVideos(skip+2)
+	pass
+
+
+def uploadVideo(file_name = ""):
+    print "uploading video "+file_name
+    ziggeo_target.videos().create({"tags":"test"}, file_name)
+
+indexVideos(0)
