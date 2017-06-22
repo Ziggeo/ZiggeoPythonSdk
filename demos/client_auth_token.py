0d48fbe45e94662dfc21c868f56ab8a8fba176f5
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-06-23 05:18:51 +0700

    add basic creation of client auth token

---
+++ demos/client_auth_token.py
@@ -0,0 +1,16 @@
+import sys
+
+from Ziggeo import Ziggeo
+
+if(len(sys.argv) < 4):
+	print "Error\n"
+	print "Usage: $>python client_auth_token.py YOUR_API_TOKEN YOUR_PRIVATE_KEY ENCRYPTION_KEY \n"
+	sys.exit()
+
+api_token = sys.argv[1]
+private_key = sys.argv[2]
+encryption_key = sys.argv[3]
+
+ziggeo = Ziggeo(api_token, private_key, encryption_key)
+
+print ziggeo.authtokens().create()
