96c613bdc70a040fbda33f7b54a45dfdbfb72708
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-06-23 05:12:56 +0700

    create basic server auth token

---
+++ demos/server_auth_token.py
@@ -0,0 +1,15 @@
+import sys
+import json, ast
+from Ziggeo import Ziggeo
+
+if(len(sys.argv) < 3):
+	print "Error\n"
+	print "Usage: $>python server_auth_token.py YOUR_API_TOKEN YOUR_PRIVATE_KEY \n"
+	sys.exit()
+
+api_token = sys.argv[1]
+private_key = sys.argv[2]
+
+ziggeo = Ziggeo(api_token, private_key)
+
+result = ziggeo.authtokens().create()
