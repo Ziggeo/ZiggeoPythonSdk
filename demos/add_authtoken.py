0fd7a61710a4a2e7fc228d7a01d247e1fd9fddd3
otavio <otaviomadalosso@gmail.com>
2017-06-23 06:53:55 +0200

    Demo add AuthToken

---
+++ demos/add_authtoken.py
@@ -0,0 +1,24 @@
+import sys
+
+from Ziggeo import Ziggeo
+
+if len(sys.argv) < 3:
+    print "Error\n"
+    print "Usage: $>python add_authtoken.py YOUR_API_TOKEN YOUR_PRIVATE_KEY\n"
+    sys.exit()
+
+api_token = sys.argv[1]
+private_key = sys.argv[2]
+
+ziggeo = Ziggeo(api_token, private_key)
+
+authtoken_permissions = {
+    'create': {
+        'session_owned': True
+    },
+    'update': {
+        'session_owned': True
+    }
+}
+
+print ziggeo.authtokens().create({'grants': authtoken_permissions})
