8e5946936ae923dc8de1539a2bf522cb03fc4d81
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-07-04 15:22:40 +0700

    adding comment

--- demos/index.py
+++ demos/index.py
@@ -10,6 +10,7 @@ if(len(sys.argv) < 3):
 api_token = sys.argv[1]
 private_key = sys.argv[2]
 ziggeo = Ziggeo(api_token, private_key)
+# 'limit' will limit how much index operation will fetch the videos. default 50 max 100
 video_list = ziggeo.videos().index({"limit":100})
 for video in video_list:
 	print video
