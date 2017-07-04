86b4faee78bc4cb2a955d8b2bf3472d59f71d43f
Ibnu Triyono <ibnu.triyono@gmail.com>
2017-07-04 15:21:35 +0700

    adding simple index operation sample

---
+++ demos/index.py
@@ -0,0 +1,15 @@
+import sys
+
+from Ziggeo import Ziggeo
+
+if(len(sys.argv) < 3):
+	print "Error\n"
+	print "Usage: $>python index.py YOUR_API_TOKEN YOUR_PRIVATE_KEY\n"
+	sys.exit()
+
+api_token = sys.argv[1]
+private_key = sys.argv[2]
+ziggeo = Ziggeo(api_token, private_key)
+video_list = ziggeo.videos().index({"limit":100})
+for video in video_list:
+	print video
