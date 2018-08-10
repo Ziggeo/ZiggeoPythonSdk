1b6fff7cd5d1ed9e1b9c19d70ebb5bfef8ea83dc
Ibnu triyono <ibnu.triyono@gmail.com>
2018-08-10 14:15:52 +0700

    adding script to update expiration days on single video

--- demos/video_expiration_days.py
+++ demos/video_expiration_days.py
@@ -2,9 +2,9 @@ import sys
 
 from Ziggeo import Ziggeo
 
-if(len(sys.argv) < 5):
-	print ("Error\n")
-	print ("Usage: $>python video_expiration_days.py YOUR_API_TOKEN YOUR_PRIVATE_KEY VIDEO_TOKEN EXPIRATION_DAYS\n")
+if(len(sys.argv) < 4):
+	print "Error\n"
+	print "Usage: $>python video_expiration_days.py YOUR_API_TOKEN YOUR_PRIVATE_KEY VIDEO_TOKEN EXPIRATION\n" # Expiration days are integer
 	sys.exit()
 
 api_token = sys.argv[1]
@@ -13,6 +13,6 @@ video_token = sys.argv[3]
 expiration_days = sys.argv[4]
 
 ziggeo = Ziggeo(api_token, private_key)
-update_arguments = {}
-update_arguments["expiration_days"] = int(expiration_days)
-print (ziggeo.videos().update(video_token, update_arguments))
+arguments={}
+arguments["expiration_days"] = expiration_days
+print ziggeo.videos().update(video_token, arguments)
