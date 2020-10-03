594f0bdcb80c60cfefc2c2b9b8e2b7aa39e9b8e8
Ibnu Triyono <ibnu.triyono@gmail.com>
2020-10-03 18:45:55 +0700

    add cdn url and signed upload to effect profile process and streams

--- ZiggeoEffectProfileProcess.py
+++ ZiggeoEffectProfileProcess.py
@@ -16,8 +16,18 @@ class ZiggeoEffectProfileProcess:
         return self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/filter', data)
 
     def create_watermark_process(self, effect_token_or_key, data = None, file = None):
-        return self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/watermark', data, file)
+        if (file != None):
+            result = self.__application.connect.postUploadJSON('/v1/effects/' + effect_token_or_key + '/process/watermark-upload-url', 'effect_process', data, file)
+            result = self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/' + result['token'] + '/confirm-watermark')
+            return result
+        else:
+            return self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/watermark', data, file)
 
     def edit_watermark_process(self, effect_token_or_key, token_or_key, data = None, file = None):
-        return self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/watermark/' + token_or_key + '', data, file)
+        if (file != None):
+            result = self.__application.connect.postUploadJSON('/v1/effects/' + effect_token_or_key + '/process/' + token_or_key + '/watermark-upload-url', 'effect_process', data, file)
+            result = self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/' + token_or_key + '/confirm-watermark')
+            return result
+        else:
+            return self.__application.connect.postJSON('/v1/effects/' + effect_token_or_key + '/process/watermark/' + token_or_key + '', data, file)
 
--- ZiggeoStreams.py
+++ ZiggeoStreams.py
@@ -10,10 +10,10 @@ class ZiggeoStreams:
         return self.__application.connect.getJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '')
 
     def download_video(self, video_token_or_key, token_or_key):
-        return self.__application.connect.get('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video')
+        return self.__application.cdn_connect.get('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video')
 
     def download_image(self, video_token_or_key, token_or_key):
-        return self.__application.connect.get('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image')
+        return self.__application.cdn_connect.get('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image')
 
     def push_to_service(self, video_token_or_key, token_or_key, data = None):
         return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/push', data)
@@ -22,13 +22,28 @@ class ZiggeoStreams:
         return self.__application.connect.delete('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '')
 
     def create(self, video_token_or_key, data = None, file = None):
-        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams', data, file)
+        if (file != None):
+            result = self.__application.connect.postUploadJSON('/v1/videos/' + video_token_or_key + '/streams-upload-url', 'stream', data, file, 'video_type')
+            result = self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + result['token'] + '/confirm-video')
+            return result
+        else:
+            return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams', data, file)
 
     def attach_image(self, video_token_or_key, token_or_key, data = None, file = None):
-        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image', data, file)
+        if (file != None):
+            result = self.__application.connect.postUploadJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image-upload-url', 'stream', data, file)
+            result = self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/confirm-image')
+            return result
+        else:
+            return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/image', data, file)
 
     def attach_video(self, video_token_or_key, token_or_key, data = None, file = None):
-        return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video', data, file)
+        if (file != None):
+            result = self.__application.connect.postUploadJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video-upload-url', 'stream', data, file, 'video_type')
+            result = self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/confirm-video')
+            return result
+        else:
+            return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/video', data, file)
 
     def attach_subtitle(self, video_token_or_key, token_or_key, data = None):
         return self.__application.connect.postJSON('/v1/videos/' + video_token_or_key + '/streams/' + token_or_key + '/subtitle', data)
