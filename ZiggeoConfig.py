daad66a5f2aee85ce773e8bd5518ae168fc99145
Ibnu Triyono <ibnu.triyono@gmail.com>
2020-10-03 18:16:23 +0700

    add cdn url on config

--- ZiggeoConfig.py
+++ ZiggeoConfig.py
@@ -4,8 +4,14 @@ class ZiggeoConfig:
         self.server_api_url = "https://srvapi.ziggeo.com"
 
         self.regions = {"r1":"https://srvapi-eu-west-1.ziggeo.com"}
+
         self.api_url = "https://api-us-east-1.ziggeo.com"
 
         self.api_regions = {"r1":"https://api-eu-west-1.ziggeo.com"}
+
+        self.cdn_url = "https://video-cdn.ziggeo.com"
+        
+        self.cdn_regions = {"r1":"https://video-cdn-eu-west-1.ziggeo.com"}
+
         self.resilience_factor = 5
         self.request_timeout = 60
